0100a0bf36c06034eecb2093ee31850ab81d791c
Denis Senkov <defendercryak@gmail.com>
2022-09-19 23:34:43 +0300

    5 пункт сделал

--- src/ru/mirea/kvbo01/senkov/Main.java
+++ src/ru/mirea/kvbo01/senkov/Main.java
@@ -5,14 +5,22 @@ public class Main {
     public static void main(String[] args) {
         Season mySeason = Season.SUMMER;
         mySeason.info();
-        System.out.println(mySeason.srtemp);
+        System.out.println("Средняя температура сезона "+mySeason.srtemp);
         mySeason.iLike();
         mySeason.getDescription();
         /////////////////////
         mySeason=Season.WINTER;
         mySeason.info();
-        System.out.println(mySeason.srtemp);
+        System.out.println("Средняя температура сезона " + mySeason.srtemp);
         mySeason.iLike();
         mySeason.getDescription();
+        System.out.println(" - - - - ПЕЧАТЬ ИНФОРМАЦИИ О ВСЕХ ВРЕМЕНАХ ГОДА - - - -");
+        for (Season d : Season.values())
+        {
+            System.out.println(d);
+            d.info();
+            System.out.println("Средняя температура сезона " + d.srtemp);
+        }
+
     }
 }
